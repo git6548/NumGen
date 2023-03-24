051b837f2889a5d2f8d4e7ac6f28cdd755227178
Stephanie <stephanieanderson5133@gmail.com>
2023-03-24 14:22:44 -0500

    two generators now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
-var numberBtnEl = $('#generate-btn');
-var numberDisplayEl = $("#number-display");
+var numberBtn1El = $('#generate-btn1');
+var numberDisplay1El = $("#number-display1");
+var numberBtn2El = $('#generate-btn2');
+var numberDisplay2El = $("#number-display2");
 
-function getNumber() {
+function getNumber1() {
     return Math.floor(Math.random() * 4) + 1;
 }
 
-numberBtnEl.on('click', function () {
-    var newNumber = getNumber();
-    numberDisplayEl.text(newNumber);
+numberBtn1El.on('click', function () {
+    var newNumber1 = getNumber1();
+    numberDisplay1El.text(newNumber1);
   });
+
+
+
+  function getNumber2() {
+    return Math.floor(Math.random() * 4) + 1;
+}
+
+numberBtn2El.on('click', function () {
+    var newNumber2 = getNumber2();
+    numberDisplay2El.text(newNumber2);
+  });
+
 
 
   // ideas for improvement:
